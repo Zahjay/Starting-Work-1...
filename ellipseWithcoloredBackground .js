33a7e2afe25d397efdfe2695d52179f254109388
Zahra Abd'Allah <zkon360@gmail.com>
2017-02-23 21:32:32 -0500

    Add files via upload

---
+++ ellipseWithcoloredBackground .js
@@ -0,0 +1,11 @@
+
+function setup() {
+   createCanvas(633, 432);
+   background (180)//made the canvas grey 
+}
+
+function draw() {
+  //trying an in line comment changed a rect to a cousin
+  ellipse(96, 94,62,62)
+
+}

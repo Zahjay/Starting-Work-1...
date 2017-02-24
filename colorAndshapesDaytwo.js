4186cf5529ee0746115d3f7c07f0c43a4f0e6bea
Zahra Abd'Allah <zkon360@gmail.com>
2017-02-23 21:37:10 -0500

    Add files via upload

---
+++ colorAndshapesDaytwo.js
@@ -0,0 +1,23 @@
+
+function setup() {
+   createCanvas(455, 470);
+   background (180)//made the canvas grey 
+}
+
+function draw() {
+  //trying an in line comment changed a rect to a cousin
+  fill(200,180,73);
+  //The order that the fill goes in matters.Color than shape.
+  ellipse(200, 190,75,75);
+  fill(23,16,35);
+  line(29,18,38,46);
+  stroke(color('red'));
+  fill(251,216,228);
+  ellipse(240,220,75,75);
+  fill(255,51,204);
+  ellipse(260,260,75,75);
+  fill(0,165,83);
+  rect(180,250,67,67);
+  stroke(color('yellow'));
+  
+}

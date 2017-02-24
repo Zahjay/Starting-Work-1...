83e8812bc0cbb3b9913b63aebcb3a64f620b052e
Zahra Abd'Allah <zkon360@gmail.com>
2017-02-23 21:42:45 -0500

    Add files via upload

---
+++ consoleCorrect.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600,400)
+  
+}
+
+function draw() {
+  fill(250,200,200);
+  ellipse(mouseX,mouseY,100,100);
+  console.log("bitcoin");
+}
+
+//trymousePressed function
+function mousePressed(){
+  background(250,250,100);
+  rect(345,45,234,345);
+  
+}
